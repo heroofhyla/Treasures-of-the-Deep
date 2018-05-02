9c0417b6227f72bfd0424893dbd7e136b9d02118
Aezart <heroofhyla@yahoo.com>
2018-05-02 08:24:49 -0700

    Enable turning

--- Scripts/Submarine.gd
+++ Scripts/Submarine.gd
@@ -11,6 +11,7 @@ export var ACCEL = 100
 export var FRICTION = 50
 var lit = false
 var last_pos = Vector2()
+var facing = 1
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -37,19 +38,23 @@ func _physics_process(delta):
 	
 	velocity -= friction
 	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		input.x -= 1
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		input.x += 1
 	
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		input.y += 1
 	
-	if Input.is_action_pressed("ui_up") && position.y > 80:
+	if Input.is_action_pressed("move_up") && position.y > 80:
 		input.y -= 1
 	
-
+	if abs(input.x) == 1 && facing != input.x:
+			facing = input.x
+			$Sprite.scale.x = facing
+			$Particles2D.position.x *= -1
+		
 	input = input.normalized() * (ACCEL * delta)
 	velocity += input
 	if velocity.length() > MAX_SPEED:
@@ -65,8 +70,9 @@ func _physics_process(delta):
 		$Particles2D.emitting = false
 	
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("toggle_light"):
 		lit = !lit
+	
 	
 	
 #func _process(delta):
